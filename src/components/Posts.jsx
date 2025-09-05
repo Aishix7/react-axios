c26416f4774cacd3471554b0848149f72eda2081
Aishix7 <hithaishi714@gmail.com>
2025-09-05 12:02:51 +0530

    added functionality to add posts(title and body)

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { getPost } from "../api/PostApi";
+import { createPost } from "../api/PostApi";
 export const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setbody] = useState("");
+
   const getPostData = async () => {
     try {
       const res = await getPost();
@@ -12,11 +16,40 @@ export const Posts = () => {
       console.error("error fetching post:", error);
     }
   };
+  const createPostData = async () => {
+    // if (!title.trim() || !body.trim()) {
+    //   alert("both title and body are required for a post");
+    //   return;
+    // }
+    try {
+      const res = await createPost({ title, body });
+      setPosts([res.data, ...posts]);
+      setTitle("");
+      setbody("");
+    } catch (error) {
+      console.log("error creating post", error);
+    }
+  };
   useEffect(() => {
     getPostData();
   }, []);
+
   return (
     <section className="section-post">
+      <h2>Create post</h2>
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="enter content here"
+        type="text"
+        value={body}
+        onChange={(e) => setbody(e.target.value)}
+      />
+      <button onClick={createPostData}>Add Post</button>
       <ul>
         {posts.map((curElement) => {
           const { id, body, title } = curElement;
